0484bd9deffa36f05e07715120f05bb2087ca954
Rafiqdeveloper <163410326+Rafiqdeveloper@users.noreply.github.com>
2024-03-15 01:24:29 +0530

    project
    project

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-secondary border text-center">
+      <p className="text-white fs-6 fw-semibold m-3">Designed by ⭐ Rafiq ⭐</p>
+    </div>
+  );
+};
+
+export default Footer;
